bd2f173fcdaf030e727fb101a84d2b901cc5f5e6
danieloh30 <doh@redhat.com>
2022-02-28 17:41:25 -0500

    Update funqy logic

--- src/test/java/org/acme/MyFunctionsTest.java
+++ src/test/java/org/acme/MyFunctionsTest.java
@@ -9,25 +9,5 @@ import static org.hamcrest.Matchers.containsString;
 
 @QuarkusTest
 public class MyFunctionsTest {
-
-    @Test
-    public void testFun() {
-        given()
-            .post("/fun")
-            .then()
-            .statusCode(200)
-            .body(containsString("Hello Funqy!"));
-    }
-
-    @Test
-    public void testFunWithName() {
-        given()
-            .contentType(ContentType.JSON)
-            .body("{\"name\": \"Friend\"}")
-            .post("/fun")
-            .then()
-            .statusCode(200)
-            .body(containsString("Hello Friend!"));
-    }
-
+     // Add test cases
 }
