f1a9465b73785d2e3a84481741d93c9169978d3c
danieloh30 <doh@redhat.com>
2022-02-28 17:42:33 -0500

    Update MyFunctions

--- src/main/java/org/acme/MyFunctions.java
+++ src/main/java/org/acme/MyFunctions.java
@@ -5,13 +5,8 @@ import io.quarkus.funqy.Funq;
 public class MyFunctions {
 
     @Funq
-    public String fun(FunInput input) {
-        return String.format("Hello %s!", input != null ? input.name : "Funqy");
-    }
-
-
-    public static class FunInput {
-        public String name;
+    public String hello() {
+        return "Welcome, Quarkus Serverless Functions on the Developer Sandbox!";
     }
 
 }
